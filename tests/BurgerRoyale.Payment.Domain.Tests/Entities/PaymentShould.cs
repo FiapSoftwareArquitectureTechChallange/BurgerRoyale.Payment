07eaae5683deaa15e80acfb843e5badf295da192
Raffael Eloi <raffaeleloi121@gmail.com>
2024-05-01 10:38:00 -0300

    Return_Notification_When_Payment_Status_Is_Invalid (RED)

--- tests/BurgerRoyale.Payment.Domain.Tests/Entities/PaymentShould.cs
+++ tests/BurgerRoyale.Payment.Domain.Tests/Entities/PaymentShould.cs
@@ -78,4 +78,41 @@ internal class PaymentShould
 
 		#endregion
 	}
+	
+	[Test]
+    public void Return_Notification_When_Payment_Status_Is_Invalid()
+    {
+		#region Arrange(Given)
+
+		var invalidStatus = PaymentStatus.None;
+
+        #endregion
+
+        #region Act(When)
+
+        var payment = new Payment(
+			Guid.NewGuid(),
+            invalidStatus,
+            50
+		);
+
+        #endregion
+
+        #region Assert(Then)
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(payment.IsValid, Is.False);
+
+            Assert.That(payment.Notifications.Count, Is.EqualTo(1));
+        });
+
+        Assert.Multiple(() =>
+		{
+			Assert.That(payment.Notifications.First().Key, Is.EqualTo("Payment Status"));
+			Assert.That(payment.Notifications.First().Message, Is.EqualTo("The Payment Status is invalid."));
+		});
+
+		#endregion
+	}
 }

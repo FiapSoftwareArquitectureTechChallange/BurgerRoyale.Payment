b13397990ee4046f2df90deef57e5750f9d561ae
Raffael Eloi <raffaeleloi121@gmail.com>
2024-05-01 10:35:25 -0300

    Return_Notification_When_Does_Not_Have_Value_Defined (RED)

--- tests/BurgerRoyale.Payment.Domain.Tests/Entities/PaymentShould.cs
+++ tests/BurgerRoyale.Payment.Domain.Tests/Entities/PaymentShould.cs
@@ -41,4 +41,41 @@ internal class PaymentShould
 
 		#endregion
 	}
+	
+	[Test]
+    public void Return_Notification_When_Does_Not_Have_Value_Defined()
+    {
+		#region Arrange(Given)
+
+		decimal invalidValue = 0;
+
+		#endregion
+
+		#region Act(When)
+
+		var payment = new Payment(
+			Guid.NewGuid(),
+			PaymentStatus.Pending,
+			invalidValue
+		);
+
+        #endregion
+
+        #region Assert(Then)
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(payment.IsValid, Is.False);
+
+            Assert.That(payment.Notifications.Count, Is.EqualTo(1));
+        });
+
+        Assert.Multiple(() =>
+		{
+			Assert.That(payment.Notifications.First().Key, Is.EqualTo("Value"));
+			Assert.That(payment.Notifications.First().Message, Is.EqualTo("The Value is required."));
+		});
+
+		#endregion
+	}
 }

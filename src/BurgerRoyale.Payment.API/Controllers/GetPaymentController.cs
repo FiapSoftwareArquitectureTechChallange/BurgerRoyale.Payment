ce90ff6ff978b66a956fe1ee4bee2a72b52838a8
Raffael Eloi <raffaeleloi121@gmail.com>
2024-05-01 17:11:12 -0300

    create GetPaymentById endpoint

--- src/BurgerRoyale.Payment.API/Controllers/GetPaymentController.cs
+++ src/BurgerRoyale.Payment.API/Controllers/GetPaymentController.cs
@@ -1,4 +1,5 @@
-﻿using BurgerRoyale.Payment.Application.Contracts.UseCases;
+﻿using BurgerRoyale.Payment.API.Extensions;
+using BurgerRoyale.Payment.Application.Contracts.UseCases;
 using BurgerRoyale.Payment.Application.Models;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
@@ -19,6 +20,27 @@ public class GetPaymentController(IGetPayment getPayment) : ControllerBase
     public async Task<ActionResult> GetPayments()
     {
         IEnumerable<GetPaymentResponse> response = await getPayment.GetAsync();
+
+        return Ok(response);
+    }
+
+    [HttpGet("{id:Guid}", Name = "GetPaymentById")]
+    [SwaggerOperation(
+        Summary = "Get payment by id",
+        Description = "Get the payment given the id.")]
+    [ProducesResponseType(typeof(GetPaymentResponse),
+        StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ValidationProblemDetails),
+        StatusCodes.Status400BadRequest)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult> GetPaymentById(Guid id)
+    {
+        GetPaymentResponse response = await getPayment.GetByIdAsync(id);
+
+        if (!response.IsValid)
+        {
+            return ValidationProblem(ModelState.AddErrosFromNofifications(response.Notifications));
+        }
 
         return Ok(response);
     }

63df771b6cd50aa9e6a961693eac86a493e576f7
Raffael Eloi <raffaeleloi121@gmail.com>
2024-05-01 16:03:54 -0300

    inject dependencies

--- src/BurgerRoyale.Payment.Application/DependencyInjection/InjectionConfig.cs
+++ src/BurgerRoyale.Payment.Application/DependencyInjection/InjectionConfig.cs
@@ -1,4 +1,6 @@
-﻿using BurgerRoyale.Payment.Application.Contracts.UseCases;
+﻿using BurgerRoyale.Payment.Application.Contracts.Mappers;
+using BurgerRoyale.Payment.Application.Contracts.UseCases;
+using BurgerRoyale.Payment.Application.Mapper;
 using BurgerRoyale.Payment.Application.UseCases;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,6 +10,17 @@ public static class InjectionConfig
 {
     public static void AddApplicationDependencies(this IServiceCollection services)
     {
+        #region UseCases
+        
         services.AddScoped<IRequestPayment, RequestPayment>();
+        services.AddScoped<IGetPayment, GetPayment>();
+        
+        #endregion
+        
+        #region Mappers
+        
+        services.AddScoped<IPaymentMapper, PaymentMapper>();
+        
+        #endregion
     }
 }

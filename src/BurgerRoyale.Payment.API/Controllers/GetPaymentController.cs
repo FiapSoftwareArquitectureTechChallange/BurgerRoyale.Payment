7131a9a3faeda48b613bf8545d63713070c9d491
Raffael Eloi <raffaeleloi121@gmail.com>
2024-05-01 16:02:17 -0300

    create GetPaymentController endpoint

---
+++ src/BurgerRoyale.Payment.API/Controllers/GetPaymentController.cs
@@ -0,0 +1,25 @@
+﻿using BurgerRoyale.Payment.Application.Contracts.UseCases;
+using BurgerRoyale.Payment.Application.Models;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace BurgerRoyale.Payment.API.Controllers;
+
+[Route("api/Payment")]
+[ApiController]
+public class GetPaymentController(IGetPayment getPayment) : ControllerBase
+{
+    [HttpGet(Name = "GetPayments")]
+    [SwaggerOperation(
+        Summary = "Get payments",
+        Description = "Get all payments.")]
+    [ProducesResponseType(typeof(IEnumerable<GetPaymentResponse>),
+        StatusCodes.Status200OK)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult> GetPayments()
+    {
+        IEnumerable<GetPaymentResponse> response = await getPayment.GetAsync();
+
+        return Ok(response);
+    }
+}

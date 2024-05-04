1a8823c0838bca31bd4e7891aa01d52466cfdd0c
Raffael Eloi <raffaeleloi121@gmail.com>
2024-05-04 12:32:33 -0300

    Validate_Payment (RED)

--- tests/BurgerRoyale.Payment.Application.Tests/Validators/MakePaymentValidatorShould.cs
+++ tests/BurgerRoyale.Payment.Application.Tests/Validators/MakePaymentValidatorShould.cs
@@ -1,16 +1,22 @@
 ﻿namespace BurgerRoyale.Payment.Application.Tests.Validators;
 
+using BurgerRoyale.Payment.Application.Contracts.Validators;
 using BurgerRoyale.Payment.Application.Models;
 using BurgerRoyale.Payment.Domain.Entities;
 using BurgerRoyale.Payment.Domain.Enums;
+using Moq;
 
 internal class MakePaymentValidatorShould
 {
+    private Mock<IPaymentValidator> paymentValidatorMock;
+    
     private IMakePaymentValidator validator;
 
     [SetUp]
     public void SetUp()
     {
+        paymentValidatorMock = new Mock<IPaymentValidator>();
+
         validator = new MakePaymentValidator();
     }
 
@@ -43,6 +49,36 @@ internal class MakePaymentValidatorShould
             Assert.That(response.Notifications.First().Key, Is.EqualTo("Payment"));
             Assert.That(response.Notifications.First().Message, Is.EqualTo("The payment has already been paid."));
         });
+
+        #endregion
+    }
+    
+    [Test]
+    public void Validate_Payment()
+    {
+        #region Arrange(Given)
+
+        Payment? unexistingPayment = null;
+
+        var invalidResponse = new NotificationModel();
+
+        paymentValidatorMock
+            .Setup(paymentValidator => paymentValidator.IsInvalid(unexistingPayment, out invalidResponse))
+            .Returns(true);
+
+        #endregion
+
+        #region Act(When)
+
+        bool isInvalid = validator.IsInvalid(unexistingPayment, out NotificationModel response);
+
+        #endregion
+
+        #region Assert(Then)
+
+        Assert.That(isInvalid, Is.True);
+
+        Assert.That(response, Is.EqualTo(invalidResponse));
 
         #endregion
     }
